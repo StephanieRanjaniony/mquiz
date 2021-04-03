fccca849e1f693591d45a5b6dbe70a6f1aa2c839
StephanieRanjaniony <81872515+StephanieRanjaniony@users.noreply.github.com>
2021-04-03 21:21:54 +0400

    HELLO

--- app/src/main/java/com/example/mquizz/q1.java
+++ app/src/main/java/com/example/mquizz/q1.java
@@ -2,13 +2,36 @@ package com.example.mquizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class q1 extends AppCompatActivity {
+    private Button btn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q1);
-    }
-}
+        btn = (Button) findViewById(R.id.btnvalider);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                Intent helloworld1 = new Intent(getApplicationContext(), q2.class);
+                startActivity(helloworld1);
+
+
+            }
+        });
+    }}
--- app/src/main/java/com/example/mquizz/q2.java
+++ app/src/main/java/com/example/mquizz/q2.java
@@ -3,12 +3,17 @@ package com.example.mquizz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class q2 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q2);
+
+
     }
 }
